66b484ccd7ceb354106f8078539cd7493c36d484
Parth Vekariya <parthvekariya260898@gmail.com>
2024-03-07 22:37:03 -0500

    - blog details ui design updates

--- blogs_demo_full_stack/src/components/BlogDetails.js
+++ blogs_demo_full_stack/src/components/BlogDetails.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col, Card, CardBody, Button } from 'reactstrap';
+import { Container, Row, Col, Card, CardBody } from 'reactstrap';
 import axios from 'axios';
 import { FaUser, FaCalendarAlt, FaTwitter, FaLinkedin, FaGithub } from 'react-icons/fa';
 import { Link, useParams } from 'react-router-dom';
+import coverImage from '../images/cover.jpg';
 
 const BlogDetails = () => {
     const { id } = useParams();
@@ -24,16 +25,14 @@ const BlogDetails = () => {
 
     return (
         <Container>
-            <Row className="mt-5 mb-4">
-                <Col>
-                    <h1>Blog Post Details</h1>
-                </Col>
-            </Row>
             {post && (
                 <Row className="justify-content-center">
                     <Col xs="8">
                         <Card>
                             <CardBody>
+                                <div className="cover-image">
+                                    <img src={coverImage} alt="Cover" className="img-fluid" />
+                                </div>
                                 <div className="d-flex align-items-center mb-3">
                                     <FaUser size={24} className="me-2" />
                                     <p className="m-0">{post.author}</p>
@@ -43,12 +42,9 @@ const BlogDetails = () => {
                                     <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" style={{ marginRight: '10px' }}><FaLinkedin /></a>
                                     <a href="https://github.com" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
                                 </div>
-                                <h5 className="card-title">{post.title}</h5>
+                                <h1>{post.title}</h1>
                                 <p className="card-text">{post.content}</p>
                                 <p className="card-text"><FaCalendarAlt className="me-2" />Created at: {post.createdAt}</p>
-                                <Link to="/dashboard">
-                                    <Button color="primary">Back to Dashboard</Button>
-                                </Link>
                             </CardBody>
                         </Card>
                     </Col>
