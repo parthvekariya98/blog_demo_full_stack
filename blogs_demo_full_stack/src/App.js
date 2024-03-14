632f028d3ad97cf71142699057b6111f2b0f46ab
Parth Vekariya <parthvekariya260898@gmail.com>
2024-03-13 21:30:44 -0400

    - managed header for specific pages ( removed from authentication pages )

--- blogs_demo_full_stack/src/App.js
+++ blogs_demo_full_stack/src/App.js
@@ -4,26 +4,23 @@ import About from './components/About';
 import BlogDetails from './components/BlogDetails';
 import Contact from './components/Contact';
 import Dashboard from './components/Dashboard';
-import Header from './components/Header';
 import Login from './components/Login';
 import Signup from './components/Signup';
+import Header from './components/Header';
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/blogdetails/:id" element={<BlogDetails />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/contact' element={<Contact />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<><Header /><Dashboard /></>} />
+        <Route path="/blogdetails/:id" element={<><Header /><BlogDetails /></>} />
+        <Route path='/about' element={<><Header /><About /></>} />
+        <Route path='/contact' element={<><Header /><Contact /></>} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
